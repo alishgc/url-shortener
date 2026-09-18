ecf4244122fa87d20eb9b650a6629442401911bc
alishgc <alishgc63@gmail.com>
2026-09-18 19:13:17 +0545

    Add css to views

--- db/db.js
+++ db/db.js
@@ -7,11 +7,6 @@ const pool = mysql.createPool({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT,
-
-    ssl: {
-        rejectUnauthorized: false
-    }
-    
 });
 
 module.exports = pool;
